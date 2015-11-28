4e99b96d5a8d59de204294013ca006e043e387ab
lduranfe <lduranfe@student.cs.uwaterloo.ca>
2015-11-27 21:55:35 -0500

    Implement Game

---
+++ game/game.cc
@@ -0,0 +1,58 @@
+#include "game.h"
+#include <vector>
+using namespace std;
+
+Game::Game() : controller(0) {
+	this->statics = new vector<StaticEntity*>();
+	this->livings = new vector<LivingEntity*>();
+}
+
+Game::~Game() {
+	for (unsigned int i = 0; i < this->statics->size(); i++) {
+		delete this->statics->at(i);
+	}
+	delete this->statics;
+
+	for (unsigned int i = 0; i < this->livings->size(); i++) {
+		delete this->livings->at(i);
+	}
+	delete this->livings;
+}
+
+int Game::getStaticAmount() const {
+	return this->statics->size();
+}
+
+int Game::getLivingAmount() const {
+	return this->livings->size();
+}
+
+StaticEntity* Game::getStaticAt(int i) const {
+	return this->statics->at(i);
+}
+
+LivingEntity* Game::getLivingAt(int i) const {
+	return this->livings->at(i);
+}
+
+GameObject* Game::addObject(Kind kind) {
+	return 0;
+}
+
+void Game::update() {
+	// TODO: Basically the entire fucking thing
+}
+
+void Game::setController(Controller* controller) {
+	this->controller = controller;
+}
+
+
+
+
+
+
+
+
+
+
